aef7ee225f11d42c6e333b733cfd9c080e42fd4d
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-10 20:28:39 +0530

    File updated at 1/10/2026, 8:28:38 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,15 +1,14 @@
-// Last updated: 1/10/2026, 8:20:01 PM
+// Last updated: 1/10/2026, 8:28:38 PM
 1class Solution {
-2    public boolean containsDuplicate(int[] nums) {
-3        Set<Integer> set = new HashSet<>();
-4
-5        for(int num : nums){
-6            if(set.contains(num)){
-7               return true;
-8            }
-9            set.add(num);
-10        }
-11
-12        return false;
-13    }
-14}
+2    public int missingNumber(int[] nums) {
+3        int n = nums.length;
+4        int expectedSum = n*(n+1)/2;
+5        int actualSum = 0;
+6        for(int num: nums){
+7            actualSum += num;
+8        
+9        }
+10       return expectedSum - actualSum;
+11        
+12    }
+13}

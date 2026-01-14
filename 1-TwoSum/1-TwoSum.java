4f989ccf7b47142b956d8815069d3d1acc95b83c
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-14 13:27:08 +0530

    File updated at 1/14/2026, 1:27:08 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,22 +1,24 @@
-// Last updated: 6/19/2025, 12:09:30 PM
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // Step 1: Create a HashMap to store number and index
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // Step 2: Loop through the array
-        for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i]; // the value we need
-
-            // Step 3: Check if the required value exists in map
-            if (map.containsKey(diff)) {
-                return new int[] { map.get(diff), i }; // return both indices
-            }
-
-            // Step 4: Store the current number and its index
-            map.put(nums[i], i);
-        }
-
-        return new int[] {}; // return empty if no pair found
-    }
-}
+// Last updated: 1/14/2026, 1:27:08 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3
+4        /*
+5        To make pairs, we need two loops.
+6        First number → pick one
+7        Second number → check with all remaining numbers
+8        second number index (always after i) -> j = i + 1
+9       */
+10
+11        for(int i =0; i< nums.length ; i++){
+12            for(int j =  i+1; j < nums.length; j++){
+13
+14                if(nums[i] + nums[j] == target){
+15                    return new int[]{i,j};
+16
+17                }
+18            }
+19        }
+20         return new int[]{-1, -1};
+21        
+22    }
+23}

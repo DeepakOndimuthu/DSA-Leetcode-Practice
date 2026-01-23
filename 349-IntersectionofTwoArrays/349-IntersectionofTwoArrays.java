5b6b6dc1aea4c69fd35649a92093f06a52b030e7
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-23 13:54:18 +0530

    File updated at 1/23/2026, 1:54:16 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -1,30 +1,28 @@
-// Last updated: 1/23/2026, 1:49:48 PM
+// Last updated: 1/23/2026, 1:54:16 PM
 1class Solution {
-2    public int[] intersection(int[] nums1, int[] nums2) {
-3        int i =0; int j=0;
+2    public int[] intersect(int[] nums1, int[] nums2) {
+3         int i =0; int j=0;
 4        Arrays.sort(nums1);
 5        Arrays.sort(nums2);
 6        
 7        List<Integer> result = new ArrayList<>();
 8        while(i < nums1.length && j < nums2.length){
 9            if(nums1[i] == nums2[j]){
-10                if(result.isEmpty() || result.get(result.size()-1) != nums1[i]){
-11                    result.add(nums1[i]);
-12                }
-13                i++;
-14                j++;
-15
-16            }else if (nums1[i] < nums2[j]){
-17                i++;
-18            }else{
-19                j++;
-20            }
-21        }
-22        int[] ans = new int[result.size()];
-23        for(int k =0; k< result.size(); k++){
-24            ans[k] = result.get(k);
-25        }
-26        return ans;
-27        
-28    }
-29}
+10                result.add(nums1[i]);
+11                i++;
+12                j++;
+13
+14            }else if (nums1[i] < nums2[j]){
+15                i++;
+16            }else{
+17                j++;
+18            }
+19        }
+20        int[] ans = new int[result.size()];
+21        for(int k =0; k< result.size(); k++){
+22            ans[k] = result.get(k);
+23        }
+24        return ans;
+25        
+26    }
+27}

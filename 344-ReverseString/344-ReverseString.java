c73ce7e974610fc98ddb37a44d1ef3933cb5b74d
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-28 17:15:06 +0530

    File updated at 1/28/2026, 5:15:07 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,17 +1,27 @@
-// Last updated: 1/28/2026, 4:58:19 PM
+// Last updated: 1/28/2026, 5:15:07 PM
 1class Solution {
-2    public void reverseString(char[] s) {
+2    public boolean isPalindrome(String s) {
 3        int left = 0;
-4        int right = s.length - 1;
-5
-6        while(left < right){
-7            char temp = s[left];
-8            s[left] = s[right];
-9            s[right] = temp;
-10
-11            left++;
-12            right--;
-13        }
-14        
-15    }
-16}
+4        int right = s.length() - 1;
+5        while(left < right){
+6
+7             // skip non-alphanumeric from left
+8            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+9                left++;
+10            }
+11            // skip non-alphanumeric from right
+12             while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+13                right--;
+14            }
+15            // compare in lowercase
+16            if(Character.toLowerCase(s.charAt(left))
+17             != Character.toLowerCase(s.charAt(right))){
+18                return false;
+19            }
+20            left++;
+21            right--;
+22        }
+23        return true;
+24        
+25    }
+26}

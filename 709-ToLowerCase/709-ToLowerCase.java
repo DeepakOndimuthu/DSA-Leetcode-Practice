38820b3eadf613126f21ac972826d3af2c26280d
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-31 16:23:27 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.java
@@ -0,0 +1,8 @@
+// Last updated: 1/31/2026, 4:23:26 PM
+1class Solution {
+2    public String toLowerCase(String s) {
+3        String lowerCase = s.toLowerCase();
+4        return lowerCase;
+5        
+6    }
+7}

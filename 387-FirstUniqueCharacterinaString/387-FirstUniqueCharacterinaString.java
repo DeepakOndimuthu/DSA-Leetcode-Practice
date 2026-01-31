9616ee25da48250b8c7257fe221c4a6f06ece0ca
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-01-31 16:40:29 +0530

    Create 387-FirstUniqueCharacterinaString

---
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -0,0 +1,18 @@
+// Last updated: 1/31/2026, 4:40:28 PM
+1class Solution {
+2    public int firstUniqChar(String s) {
+3        int [] freq = new int[26];
+4
+5        for(int i = 0; i< s.length();i++){
+6            freq[s.charAt(i) - 'a']++;
+7        }
+8        for(int i = 0; i< s.length();i++){
+9            if(freq[s.charAt(i) - 'a'] == 1){
+10                return i;
+11            }
+12        }
+13
+14        return -1;
+15        
+16    }
+17}

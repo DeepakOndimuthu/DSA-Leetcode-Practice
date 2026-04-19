8eebebe1955c768e90e9ee0b2b6eb84a95d2eb0d
Deepak Ondimuthu <37857985+DeepakOndimuthu@users.noreply.github.com>
2026-04-19 19:46:29 +0530

    File updated at 4/19/2026, 7:46:29 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,4 +1,4 @@
-// Last updated: 2/24/2026, 3:54:16 PM
+// Last updated: 4/19/2026, 7:46:29 PM
 1class Solution {
 2    public int subarraySum(int[] nums, int k) {
 3
@@ -22,33 +22,43 @@
 21    }
 22}
 23
-24/*
-25HashSet only tells: Seen or not seen
-26here we must: Count how many times So we use HashMap
+24    /*
+25    HashSet only tells: Seen or not seen
+26    here we must: Count how many times So we use HashMap
 27
-28List all subarrays:EX1
-29[1]        → 1
-30[1,1]      → 2  ✅
-31[1,1,1]    → 3
-32   [1]     → 1
-33   [1,1]   → 2  ✅
-34      [1]  → 1
+28    List all subarrays:EX1
+29    [1]        → 1
+30    [1,1]      → 2  ✅
+31    [1,1,1]    → 3
+32    [1]     → 1
+33    [1,1]   → 2  ✅
+34        [1]  → 1
 35
-36Example 2
-37[1] → 1
-38[1,2] → 3 ✅
-39[1,2,3] → 6
-40   [2] → 2
-41   [2,3] → 5
-42      [3] → 3 ✅
-43
-44we store sums {0→1, 1→1, 3→1} ✅
-45not values {1→1, 2→1, 3→1}   ❌
-46
-47we remove this line
-48map.put(0,1);
-49sum-k = 3-3 = 0
-50map.containsKey(0) ? so why 
-51we MISS subarray [1,2]
-52
-53*/
+36    Valid ones
+37    [1,1] (index 0–1)
+38    [1,1] (index 1–2)
+39    Count = 2
+40
+41    Example 2
+42    [1] → 1
+43    [1,2] → 3 ✅
+44    [1,2,3] → 6
+45    [2] → 2
+46    [2,3] → 5
+47        [3] → 3 ✅
+48
+49    Valid ones
+50    [1,2]
+51    [3]
+52    Count = 2
+53
+54    we store sums {0→1, 1→1, 3→1} ✅
+55    not values {1→1, 2→1, 3→1}   ❌
+56
+57    we remove this line
+58    map.put(0,1);
+59    sum-k = 3-3 = 0
+60    map.containsKey(0) ? so why 
+61    we MISS subarray [1,2]
+62
+63    */
